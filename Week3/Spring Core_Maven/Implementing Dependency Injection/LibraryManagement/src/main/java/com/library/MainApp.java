8aef0f75cbcb1b36e839a3a6209cd108e09218ed
Kirthana A <kirthanaaa9@gmail.com>
2025-07-03 21:18:22 +0530

    week3_2

---
+++ Week3/Spring Core_Maven/Implementing Dependency Injection/LibraryManagement/src/main/java/com/library/MainApp.java
@@ -0,0 +1,18 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Load Spring ApplicationContext from XML
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Call a method to test dependency injection
+        bookService.showBooks();
+    }
+}
